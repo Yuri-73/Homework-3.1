34370f10e13373222586c1ae6d40f4a541fda317
Юрий <fedejkin@gmail.com>
2023-02-15 21:12:52 +0300

    task-1 + task-2 + task-3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -28,5 +29,22 @@ public class Main {
             else if (clientOS == 1)
                 System.out.println("Установите версию приложения для Android по ссылке");
         }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        int year = 2004;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0)
+                    System.out.println(year + " год является високосным");
+                else
+                    System.out.println(year + " год не является високосным");
+            }
+            else
+               System.out.println(year + " год является високосным");
+        }
+        else
+            System.out.println(year + " год не является високосным");
     }
 }
